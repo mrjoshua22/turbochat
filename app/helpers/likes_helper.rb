7ff59a3382a7138e3de0f3e9a165c2266fda1a6e
mrjoshua22 <ulanov.ng@gmail.com>
2023-09-27 20:06:04 +0300

    Add likes count to message partial

---
+++ app/helpers/likes_helper.rb
@@ -0,0 +1,14 @@
+module LikesHelper
+  def likes(message, user)
+    heart =
+      if message.likes.find_by(user: user).present?
+        "❤"
+      else
+        "🤍"
+      end
+
+    heart << " #{message.likes_count}" if message.likes_count.positive?
+
+    heart
+  end
+end
