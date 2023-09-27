2093a7e385a8cf5c43e0b7ba353ad417f30a534f
mrjoshua22 <ulanov.ng@gmail.com>
2023-09-27 19:04:03 +0300

    Add Room and Message controllers with turbo

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,8 @@ class Room < ApplicationRecord
   has_many :messages, -> { sorted }, dependent: :destroy
 
   before_create { self.title = SecureRandom.hex(3) }
+
+  def to_param
+    title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'rooms#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
+
+  resources :messages, only: :create
+  resources :rooms, only: %i[create index show], param: :title
 end
